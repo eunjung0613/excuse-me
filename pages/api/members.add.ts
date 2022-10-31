ae9bf03712da4586e88a6710d492490311c88bb7
EunJungKim <dmswjd7281@naver.com>
2022-10-31 23:09:55 +0900

    4.2 user API modify

--- pages/api/members.add.ts
+++ pages/api/members.add.ts
@@ -8,18 +8,32 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (uid === undefined || uid === null) {
     return res.status(400).json({ result: false, message: 'uid가 누락되었습니다.' });
   }
-
+  if (email === undefined || email === null) {
+    return res.status(400).json({ result: false, message: 'email이 누락되었습니다.' });
+  }
   try {
-    const addResult = await FirebaseAdmin.getInstance()
-      .Firebase.collection('members')
-      .doc(uid)
-      .set({
+    const screenName = (email as string).replace('@gmail.com', '');
+    const addResult = await FirebaseAdmin.getInstance().Firebase.runTransaction(async (transaction) => {
+      const memberRef = FirebaseAdmin.getInstance().Firebase.collection('members').doc(uid);
+      const screenNameRef = FirebaseAdmin.getInstance().Firebase.collection('screen_names').doc(screenName);
+      const memberDoc = await transaction.get(memberRef);
+      if (memberDoc.exists) {
+        return false;
+      }
+      const addData = {
         uid,
-        email: email ?? '',
+        email,
         displayName: displayName ?? '',
         photoURL: photoURL ?? '',
-      });
-    return res.status(200).json({ result: true, id: addResult });
+      };
+      await transaction.set(memberRef, addData);
+      await transaction.set(screenNameRef, addData);
+      return true;
+    });
+    if (addResult === false) {
+      return res.status(201).json({ result: true, id: uid });
+    }
+    return res.status(200).json({ result: true, id: uid });
   } catch (err) {
     console.error(err);
     res.status(500).json({ result: false });
