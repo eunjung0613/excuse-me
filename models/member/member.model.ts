bdbc19a2cc944fbdaa2d5bc8beac31a5dcfa9375
EunJungKim <dmswjd7281@naver.com>
2022-11-01 00:01:28 +0900

    4.3 Add user controller

---
+++ models/member/member.model.ts
@@ -0,0 +1,42 @@
+import { InAuthUser } from '../in_auth_user';
+import FirebaseAdmin from '@/models/firebase_admin';
+
+const MEMVER_COL = 'members';
+const SCR_NAME_COL = 'screen_names';
+type AddResult = { result: true; id: string } | { result: false; message: string };
+
+async function add({ uid, displayName, email, photoURL }: InAuthUser): Promise<AddResult> {
+  try {
+    const screenName = (email as string).replace('@gmail.com', '');
+    const addResult = await FirebaseAdmin.getInstance().Firebase.runTransaction(async (transaction) => {
+      const memberRef = FirebaseAdmin.getInstance().Firebase.collection('members').doc(uid);
+      const screenNameRef = FirebaseAdmin.getInstance().Firebase.collection('screen_names').doc(screenName);
+      const memberDoc = await transaction.get(memberRef);
+      if (memberDoc.exists) {
+        return false;
+      }
+      const addData = {
+        uid,
+        email,
+        displayName: displayName ?? '',
+        photoURL: photoURL ?? '',
+      };
+      await transaction.set(memberRef, addData);
+      await transaction.set(screenNameRef, addData);
+      return true;
+    });
+    if (addResult === false) {
+      return { result: true, id: uid };
+    }
+    return { result: true, id: uid };
+  } catch (err) {
+    console.error(err);
+    return { result: false, message: '서버에러' };
+  }
+}
+
+const MemberModel = {
+  add,
+};
+
+export default MemberModel;
--- pages/api/members.add.ts
+++ pages/api/members.add.ts
@@ -1,41 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import FirebaseAdmin from '@/models/firebase_admin';
+import memberCtrl from '@/controllers/member.ctr';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { uid, email, displayName, photoURL } = req.body;
-  if (uid === undefined || uid === null) {
-    return res.status(400).json({ result: false, message: 'uid가 누락되었습니다.' });
-  }
-  if (email === undefined || email === null) {
-    return res.status(400).json({ result: false, message: 'email이 누락되었습니다.' });
-  }
+  const { method } = req;
+  const supportMethod = ['POST'];
   try {
-    const screenName = (email as string).replace('@gmail.com', '');
-    const addResult = await FirebaseAdmin.getInstance().Firebase.runTransaction(async (transaction) => {
-      const memberRef = FirebaseAdmin.getInstance().Firebase.collection('members').doc(uid);
-      const screenNameRef = FirebaseAdmin.getInstance().Firebase.collection('screen_names').doc(screenName);
-      const memberDoc = await transaction.get(memberRef);
-      if (memberDoc.exists) {
-        return false;
-      }
-      const addData = {
-        uid,
-        email,
-        displayName: displayName ?? '',
-        photoURL: photoURL ?? '',
-      };
-      await transaction.set(memberRef, addData);
-      await transaction.set(screenNameRef, addData);
-      return true;
-    });
-    if (addResult === false) {
-      return res.status(201).json({ result: true, id: uid });
+    if (supportMethod.indexOf(method!) === -1) {
+      //에러 반환
     }
-    return res.status(200).json({ result: true, id: uid });
+    await memberCtrl.add(req, res);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ result: false });
+    //에러 처리
   }
 }
